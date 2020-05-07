ababc4e7a06216eb61becca2d6941f21f3e6fb2b
Stefan Gherman <scgheman@gmail.com>
2020-05-07 17:45:09 +0300

    Add tests for FilePartReader Class

--- src/main/java/com/codecool/readers/FilePartReader.java
+++ src/main/java/com/codecool/readers/FilePartReader.java
@@ -1,51 +1,104 @@
 package com.codecool.readers;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
-//
-
+/*
+FilePartReader class that handles reading from text files
+ */
 public class FilePartReader {
 
+    /*
+    File path as a string
+     */
     private String filePath;
+
+    /*
+    The line from which the reader should perform its action
+    */
     private int fromLine;
+
+    /*
+   The line at which the reader should stop.
+    */
     private int toLine;
 
     public FilePartReader() {
 
     }
 
-    public void setFromLine (int fromLine) throws IllegalArgumentException {
-        if(fromLine > this.toLine) {
+    /*
+    <p>Public setter for the starting line </p>
+    @param fromLine the line at which the reader should start
+    @return nothing
+    @throws IllegalArgumentException when fromLine is greater than toLine
+     */
+    public void setFromLine(int fromLine) throws IllegalArgumentException {
+        if (fromLine > this.toLine || fromLine < 1) {
             throw new IllegalArgumentException();
         }
         this.fromLine = fromLine;
     }
 
-    public void setToLine (int toLine) throws IllegalArgumentException{
-        if(toLine < this.fromLine) {
+    /*
+    <p>Public setter for the ending line </p>
+    @param toLine the line at which the reader should stop
+    @return nothing
+    @throws IllegalArgumentException when fromLine is greater than toLine
+     */
+    public void setToLine(int toLine) throws IllegalArgumentException {
+        if (toLine < this.fromLine) {
             throw new IllegalArgumentException();
         }
         this.toLine = toLine;
     }
 
+    /*
+  <p>Public setter for the file path </p>
+  @param filePath actual path of the file.
+  @return nothing
+   */
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /*
+    <p> Getter for fromLine
+    @return int fromLine
+     */
     public int getFromLine() {
         return this.fromLine;
     }
 
+    /*
+  <p> Getter for fromLine
+  @return int fromLine
+   */
     public int getToLine() {
         return this.toLine;
     }
 
-
+    /*
+    <p> Getter for the filePath
+    @return filePath String the file path of the file
+     */
+    public String getFilePath() {
+        return this.filePath;
+    }
+    /*
+    <p> The setup method assigns values to the class variables
+    @param filePath the path of the file
+    @param fromLine the starting line for the reader
+    @param toLine the ending line for the reader
+    @return nothing
+    @throws IllegalArgumentException when fromLine is smaller than 1 or greater tha toLIne
+     */
     public void setup(String filePath, int fromLine, int toLine) throws IllegalArgumentException {
 
         if (fromLine > toLine || fromLine < 1) {
@@ -56,6 +109,12 @@ public class FilePartReader {
         this.fromLine = fromLine;
         this.toLine = toLine;
     }
+
+    /*
+    <p> The read method reads all the contents from a file
+    @return returnString String the result as a string representation
+    @throws IOException when file is not found
+     */
 
     public String read() throws IOException {
         List<String> fileContents = new ArrayList<>();
@@ -71,8 +130,15 @@ public class FilePartReader {
         for (String line : fileContents) {
             returnString = returnString.concat(line + '\n');
         }
-        return returnString;
+        return returnString.stripTrailing();
     }
+
+    /*
+    <p>This method reads only the line between fromLine to toLine (both included)</p>
+    @return returnString String the result as a string representation
+    @throws IOException on file not found
+    @throws IndexOutOfBoundsException  when fromLine is greater than the total number of lines the file has
+     */
 
     public String readLines() throws IOException, IndexOutOfBoundsException {
 
@@ -81,18 +147,18 @@ public class FilePartReader {
         int fileLines = returnFromRead.size();
         StringBuilder returnString = new StringBuilder();
 
-        if(this.fromLine > fileLines){
+        if (this.fromLine > fileLines) {
             System.out.println("The Starting Line (get it?) is out of bounds");
             throw new IndexOutOfBoundsException();
         } else if (this.toLine > fileLines) {
-            for(int i = fromLine - 1; i < fileLines; i++ ) {
+            for (int i = fromLine - 1; i < fileLines; i++) {
                 returnString.append(returnFromRead.get(i) + "\n");
             }
         } else {
-            for(int i = this.fromLine - 1; i < this.toLine; i++ ) {
+            for (int i = this.fromLine - 1; i < this.toLine; i++) {
                 returnString.append(returnFromRead.get(i) + "\n");
             }
         }
-        return returnString.toString();
+        return returnString.toString().stripTrailing();
     }
 }
--- src/test/java/FileReaderTest.java
+++ src/test/java/FileReaderTest.java
@@ -1,2 +1,91 @@
+import com.codecool.readers.FilePartReader;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class FileReaderTest {
+    @Test
+    public void testSetFromLineThrowsIllegalArgumentExceptionOnNegativeNumberAssignment() {
+        FilePartReader reader = new FilePartReader();
+        assertThrows(IllegalArgumentException.class, () -> {reader.setToLine(-12);});
+    }
+
+    @Test
+    public void testSetFromLineThrowsIllegalArgumentExceptionOnFromLineGreaterThanToLine() {
+        FilePartReader reader = new FilePartReader();
+        assertThrows(IllegalArgumentException.class, () -> {reader.setToLine(13); reader.setFromLine(15);});
+    }
+
+    @Test
+    public void testSetupThrowsIllegalArgumentExceptionOnNegativeNumberAssignment() {
+        FilePartReader reader = new FilePartReader();
+        assertThrows(IllegalArgumentException.class, () -> {reader.setup("path", -1, 13);});
+    }
+
+    @Test
+    public void testSetupThrowsIllegalArgumentExceptionOnToLineSmallerThanFromLine() {
+        FilePartReader reader = new FilePartReader();
+        assertThrows(IllegalArgumentException.class, () -> {reader.setup("path", 13, 8);});
+    }
+
+    @Test
+    public void testReadReturnsIOExceptionOnFIleNotFound() {
+        FilePartReader reader = new FilePartReader();
+        reader.setup("path", 1, 13);
+        assertThrows(IOException.class, reader::read);
+    }
+
+    @Test
+    public void testReadReturnsCorrectResult() throws IOException {
+        FilePartReader reader = new FilePartReader();
+        reader.setup("/mnt/7d45c543-fc06-4310-b70a-2a9aa2e43a54/Projects/codecool/java/testing_junit/.idea/files/for_test.txt", 1, 13);
+        String correctResult = "We've reached the crossroads of what makes truth and lies\n" +
+                "No more time will pass by, we need to draw the line\n" +
+                "All the things you did, they left me so betrayed\n" +
+                "The words you never said now begin to fade away";
+        assertEquals(correctResult, reader.read());
+    }
+
+    @Test
+    public void testReadLinesReturnsIOExceptionOnFileNotFound() {
+        FilePartReader reader = new FilePartReader();
+        reader.setup("path", 1, 13);
+        assertThrows(IOException.class, reader::readLines);
+    }
+
+    @Test
+    public void testReadLinesReturnsIllegalArgumentExceptionOnBadfromLineAndtoLIne() {
+        FilePartReader reader = new FilePartReader();
+        reader.setup("/mnt/7d45c543-fc06-4310-b70a-2a9aa2e43a54/Projects/codecool/java/testing_junit/.idea/files/for_test.txt", 50, 75);
+        assertThrows(IndexOutOfBoundsException.class, reader::readLines);
+    }
+
+
+    @Test
+    public void testReadLinesReturnsCorrectResult() throws IOException {
+        FilePartReader reader = new FilePartReader();
+        reader.setup("/mnt/7d45c543-fc06-4310-b70a-2a9aa2e43a54/Projects/codecool/java/testing_junit/.idea/files/for_test.txt", 2, 4);
+        String correctResult = "No more time will pass by, we need to draw the line\n" +
+                "All the things you did, they left me so betrayed\n" +
+                "The words you never said now begin to fade away";
+        assertEquals(correctResult, reader.readLines());
+    }
+
+
+
+    @Test
+    public void testReadLinesReturnsCorrectAnswerIfFromLineIsGreaterThanTheMax() throws IOException {
+        FilePartReader reader = new FilePartReader();
+        reader.setup("/mnt/7d45c543-fc06-4310-b70a-2a9aa2e43a54/Projects/codecool/java/testing_junit/.idea/files/for_test.txt", 2, 18);
+        String correctResult = "No more time will pass by, we need to draw the line\n" +
+                "All the things you did, they left me so betrayed\n" +
+                "The words you never said now begin to fade away";
+        assertEquals(correctResult, reader.readLines());
+    }
+
+
+
 }
